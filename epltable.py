0760051f68daf7492b5aadcd7a8309a9692ea9a3
Troy Ramsay <tramsay3@gmail.com>
2021-02-21 09:59:10 -0600

    A way to scrape the web using the BeautifulSoup library to re-create the current table for the English Premier League

---
+++ epltable.py
@@ -0,0 +1,23 @@
+#! python3
+
+import requests 
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+# Load the webpage content
+r = requests.get('https://www.premierleague.com/tables') 
+
+
+# Convert to beautiful soup object
+soup = BeautifulSoup(r.content, features='lxml')
+
+# removing all the extra tags at once - then passing the soup object directly to pandas
+class_names = [ 'revealMore', 'short', 'thShort', 'linkable', 'expandable', 'movement' ]
+
+for tag in soup.find_all(class_=class_names):
+    tag.decompose()
+
+df = (pd.read_html(str(soup))[0]) 
+print(pd)
+
